8cd11e2ebc5d41d74f8bbc2853f0c3ea3d710f08
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-06-25 01:47:00 +1200

    feat: add basic setup for nextjs and payload

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your Next.js config here
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  output: "standalone",
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+};
+
+export default withPayload(nextConfig, { devBundleServerPackages: false });
