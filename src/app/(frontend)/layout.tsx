dada9c7bff2746da3a424d8999ba8d5f4f0860a1
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-06-25 18:49:35 +1200

    docs: update metadata description and title

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,8 +2,17 @@ import type React from "react"
 import "./styles.css"
 
 export const metadata = {
-	description: "A blank template using Payload in a Next.js app.",
-	title: "Payload Blank Template",
+	description: "A motivated and determined Computer Science graduate student. ",
+	title: {
+		template: "%s | Jeffery Ji",
+		default: "Portfolio",
+	},
+	openGraph: {
+		title: "Portfolio | Jeffery Ji",
+		description: "A motivated and determined Computer Science graduate student. ",
+		siteName: "Portfolio | Jeffery Ji",
+		type: "website",
+	},
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
@@ -11,7 +20,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
 	return (
 		<html lang="en">
-			<body>
+			<body suppressHydrationWarning>
 				<main>{children}</main>
 			</body>
 		</html>
